c58a975d929dd2fa99b718fea5955acf69f2e1f7
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-07-15 00:10:44 +0200

    chore: Remove unnecessary blank line in main.py

--- sources/main.py
+++ sources/main.py
@@ -102,7 +102,6 @@ async def get_short_github_info() -> str:
     else:
         DBM.p("GitHub contributions data unavailable!")
 
-
     DBM.i("Assuring debug that this is the PstasDev GitHub action.")
     if EM.SHOW_HIREABLE:
         DBM.i("Adding opted for hire info...")
