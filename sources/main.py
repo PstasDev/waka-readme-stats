7363852da5dbc801313a3c3a2103291fde7cee8b
Pstas <60182573+PstasDev@users.noreply.github.com>
2024-07-15 18:14:27 +0200

    `chore: Update disk usage calculation in main.py`

--- sources/main.py
+++ sources/main.py
@@ -89,7 +89,7 @@ async def get_short_github_info() -> str:
         DBM.p("Please add new github personal access token with user permission!")
     else:
         if EM.TOTAL_DISK_SPACE:
-            disk_usage = f"{naturalsize(GHM.USER.disk_usage)}/{EM.TOTAL_DISK_SPACE} GB Used in GitHub Storage ({round(float(GHM.USER.disk_usage / (EM.TOTAL_DISK_SPACE * 1000000000) * 100))}% of the total disk space available for me Locally)"
+            disk_usage = f"{naturalsize(GHM.USER.disk_usage)}/{EM.TOTAL_DISK_SPACE} GB Used in GitHub Storage ({round(float(GHM.USER.disk_usage / (EM.TOTAL_DISK_SPACE * 1000000000) * 100), 3)}% of the total disk space available for me Locally)"
         else:
             disk_usage = "Used in GitHub's Storage" % naturalsize(GHM.USER.disk_usage)
             
